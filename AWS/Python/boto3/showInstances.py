617b81a895573ba450aa80d6754eaa90a12f3305
krestix <bishofkristina@gmail.com>
2019-07-17 02:56:07 +0300

    Short script to print AWS instances name, without using filters only for loops script name:showInstances.py

--- AWS/Python/boto3/showInstances.py
+++ AWS/Python/boto3/showInstances.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
 
+#short script to print list of my aws instances name
 import boto3
 
-ec2 = boto3.resource('ec2')
-ec2client = boto3.client('ec2')
+client=boto3.client('ec2','us-east-2')
+response = client.describe_instances()
 
-#instances = ec2.instances.filter()
-response = ec2client.describe_instances()
-for reservation in response["Reservations"]:
-    for instance in reservation["Instances"]:
-        # This sample print will output entire Dictionary object
-        print(instance)
-	print(ec2.Instance(instance["InstanceId"]))
+for reservation in (response["Reservations"]):
+	for instance in reservation["Instances"]:
+		for name in instance['Tags']:
+			if(name['Key'] == 'Name'):
+				print(name['Value'])
 
-
-
-
-#for instance in instances:
-#	for tag in instance.tags:
-#		if 'Name'in tag['Key']:
-#			print(tag['Value'])
-#for instance in instances:
- # print(instance.id, instance.instance_type, instance.tags)
